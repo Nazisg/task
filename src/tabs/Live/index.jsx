b43f8fd1ef17eda7b5c6af1ec5af11b8a06f1323
Nazisg <isgnezrin@gmail.com>
2024-03-28 21:15:07 +0400

    updated

--- src/tabs/Live/index.jsx
+++ src/tabs/Live/index.jsx
@@ -1,11 +1,28 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 import vector from '../../assets/imgs/vector.png'
 import rus from '../../assets/imgs/rus.png'
 import aze from '../../assets/imgs/aze.png'
 import styles from './Live.module.scss'
 import fe from '../../assets/imgs/floor-exercise.png'
+import axios from 'axios';
 
 export default function Live() {
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+      const fetchData = async () => {
+        try {
+          const response = await axios.get('http://localhost:3000/liveTable');
+          setData(response.data);
+  
+        } catch (error) {
+          console.error('Error fetching data:', error);
+        }
+      };
+  
+      fetchData();
+    }, []);
+    console.log(data)
     return (
         <div className="tableContainer">
             <table>
@@ -24,7 +41,7 @@ export default function Live() {
                 <tbody>
                     <tr>
                         <td><img className='apparat' src={fe} alt="floor-exercise" /></td>
-                        <td><div className="team"><img src={aze} alt="rus" /><span>AZE</span></div></td>
+                        <td><div className="team"><img className='flag' src={aze} alt="flag" /><span>AZE</span></div></td>
                         <td>212</td>
                         <td>Amirbekov T.</td>
                         <td><div className='team score'><span>D: 5.400</span><span>E: 8.800</span><span>P: 0.3</span></div></td>
